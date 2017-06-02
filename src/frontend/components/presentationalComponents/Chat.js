235ec9ebaf303c367c7c5f586e99a68052331b6b
Eric Abenojar <abenoeric@gmail.com>
2017-06-01 19:27:25 -0600

    working on css for chat

--- src/frontend/components/presentationalComponents/Chat.js
+++ src/frontend/components/presentationalComponents/Chat.js
@@ -23,6 +23,7 @@ const Chat = ({
         <div onClick={() => minimizeChat(chatIndex)} className="chat_left_arrow">
           <img alt="" src="assets/left-arrow.svg" className="icon" />
         </div>
+
         <div className="chat_top">
           <img
             alt=""
@@ -36,24 +37,31 @@ const Chat = ({
         </div>
       </div>
       <MessagesBoxContainer chatIndex={chatIndex} />
-      <div className="wrapper3">
-        <div className="respond_body">
-          <textarea
-            onKeyPress={(e) => { if (e.key === 'Enter') { clickSend(); e.preventDefault(); } }}
-            className="chat_response_body"
-            placeholder="Respond here..."
-            name="comment"
-            ref={(node) => { textArea = node; }}
-          />
+      <div className="row row3">
+        <div className="col-md-8 col-sm-12 col-xs-12 send_row">
+          <div className="main_message">
+            <textarea
+              onKeyPress={(e) => { if (e.key === 'Enter') { clickSend(); e.preventDefault(); } }}
+              className="chat_response_body"
+              placeholder="Respond here..."
+              name="comment"
+              ref={(node) => { textArea = node; }}
+            />
+          </div>
+        </div>
+        <div className="col-md-2 col-sm-6 col-xs-12 send_row">
           <div className="send_message" onClick={() => clickSend()}>
             <div className="message_text">Send</div>
           </div>
+        </div>
+        <div className="col-md-2 col-sm-6 col-xs-12 send_row">
           <div className="send_message" onClick={() => endChatClick()}>
             <div className="message_text">End</div>
           </div>
         </div>
       </div>
     </div>
+
 
   );
 };
