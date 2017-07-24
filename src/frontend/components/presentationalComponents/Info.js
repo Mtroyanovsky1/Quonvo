bfd726031e3dcbcac57953427c78abc6da4b0b9c
Mitchell Troyanovsky <mtroyanovsky1@babson.edu>
2017-07-23 20:02:39 -0400

    stuff

--- src/frontend/components/presentationalComponents/Info.js
+++ src/frontend/components/presentationalComponents/Info.js
@@ -4,7 +4,7 @@ const Info = () => (
   <div className="info_full">
     <h1 className="info_title"> Welcome to Quonvo </h1>
     <p className="info_text">
-    Welcome to Quonvo time for FME. Any questions you have, make sure to ask them. And if you can answer a question. Click on it and have a conversation.
+    Quonvo time for FME has finished! Thanks to everyone who participated. Some of the Quonvos that took place are available in the archives tab
     </p>
     <p className="info_text">
     The person who answers the most questions and gets rated the highest will win a $10 Amazon gift card
